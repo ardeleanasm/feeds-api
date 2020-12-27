85eaf29a753e92334ba8a41ba0e43e9d78a5a636
ardeleanasm <ardeleanasm@gmail.com>
2020-12-27 17:52:24 +0200

    changed  interfaces

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -9,56 +9,59 @@ import { Items } from './items.interface';
  * In-Memory Store
  */
 const items: Items = {
-    1: {
-        id: 1,
-        title: "Title1",
-        url: "Url1"
-    },
-    2: {
-        id: 2,
-        title:"Title2",
-        url:"Url2"
-    }
-}
+  numberOfEntries: 3,
+  entries: [
+    { id: 1, title: 'Title1', url: 'url1' },
+    { id: 2, title: 'Title2', url: 'url2' },
+    { id: 3, title: 'Title3', url: 'url3' }
+  ]
+};
+
+
 /**
  * Service Methods
  */
-export const findAll = async(): Promise<Items>=>{
-    return items;
+export const findAll = async (): Promise<Items> => {
+  return items;
 }
 
-export const find = async (id:number):Promise<Item>=>{
-    const record:Item = items[id];
-    if (record) {
-        return record;
-    }
-    throw new Error("No record found");
+export const find = async (id: number): Promise<Item> => {
+  const record = items.entries.find(element => element.id === id);
+
+
+  // const record:Item = items[id];
+  if (record) {
+    return record;
+  }
+  throw new Error("No record found");
 }
 
 export const create = async (newItem: Item): Promise<void> => {
-    const id = new Date().valueOf();
-    items[id] = {
-      ...newItem,
-      id
-    };
-  };
+  items.entries.push(newItem);
+  items.numberOfEntries += 1;
+
+};
 
 export const update = async (updatedItem: Item): Promise<void> => {
-    if (items[updatedItem.id]) {
-      items[updatedItem.id] = updatedItem;
-      return;
-    }
-  
-    throw new Error("No record found to update");
-  };
+  const objIndex = items.entries.findIndex(element => element.id == updatedItem.id);
+  if (objIndex != -1) {
+    items.entries[objIndex] = updatedItem;
+    return;
+  }
 
-  export const remove = async (id: number): Promise<void> => {
-    const record: Item = items[id];
+  throw new Error("No record found to update");
+};
+
+export const remove = async (id: number): Promise<void> => {
   
-    if (record) {
-      delete items[id];
-      return;
-    }
+  const objIndex = items.entries.findIndex(element => element.id == id);
   
-    throw new Error("No record found to delete");
-  };
+  
+  if (objIndex != -1) {
+    items.entries.splice(objIndex,1);
+    
+    return;
+  }
+
+  throw new Error("No record found to delete");
+};
